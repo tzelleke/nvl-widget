afdef7def353b03552870a56e53e786bffbf5c04
tzelleke <theodros.zelleke@gmail.com>
2026-01-20 09:28:51 +0100

    feat: demoes counter widget in marimo

---
+++ demo_marimo.py
@@ -0,0 +1,17 @@
+import marimo
+
+__generated_with = "0.19.4"
+app = marimo.App(width="medium")
+
+with app.setup:
+    import nvl_widget
+
+
+@app.cell
+def _():
+    nvl_widget.CounterWidget()
+    return
+
+
+if __name__ == "__main__":
+    app.run()
